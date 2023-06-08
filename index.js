76934eb7edda8eef03a6acc3119d7aef7d22fdc4
Felipe Mendoza <felipe188.mendoza@gmail.com>
2023-06-08 01:49:14 -0500

    validacion y obtencin de datos de footbalia

--- index.js
+++ index.js
@@ -12,19 +12,44 @@ app.listen(PORT,()=>{
 async function openWebPage() {
     const browser = await puppeteer.launch({
       headless: false,
-      slowMo: 300,
+      slowMo: 100,
     });
     const page = await browser.newPage();
     await page.goto("https://footballia.net/users/sign_in");
     await page.click('button.btn-close.btn.btn-base');
-    const user_email = await page.$("input#user_email");
-    await user_email.click();
-    await user_email.type('felipe188.mendoza@gmail.com');
-    const user_password = await page.$("input#user_password");
-    // await user_email.type(String.fromCharCode(13));
-    await user_email.click();
-    await user_password.type('chepita123789');
-    // await browser.close();
+    await page.evaluate(()=>{
+      //
+      document.querySelector("#user_email").value = 'felipe188.mendoza@gmail.com';
+      document.querySelector("#user_password").value = 'chepita123789';
+      //
+      const submitButton = document.querySelector('input[type="submit"]');
+      submitButton.click();
+      
+    });
+    await page.waitForNavigation();
+    await page.goto('https://footballia.net/competitions/champions-league');
+    // OBTENEMOS LA INFORMACION DE LA TABLA
+    let jsondata = await page.evaluate(()=>{
+      let tableList = document.querySelector("table.table.table-striped > tbody").children;
+      let listdata = [];
+      for(let item of tableList){
+        let date = new Date((item.children[0]).innerHTML);
+        const fechaFormateada = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+        listdata.push(
+          {
+            date:fechaFormateada, 
+            competion:item.children[2].innerHTML,
+            season:item.children[4].innerHTML,
+            stage:item.children[3].innerHTML,
+            title_match:(item.children)[1].children[1].children[0].text,
+            path_video:(item.children)[1].children[1].children[0].href
+          }
+        );
+      }
+      return listdata;
+    });
+    console.log(jsondata);
+    // BUTON
 }
   
 openWebPage();
